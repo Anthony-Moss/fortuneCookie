11f0e1a805ac7d9bfac5a0f25461b42146a7792b
Anthony Moss <asmoss93@gmail.com>
2019-03-08 20:14:03 -0500

    fixes script so now on button press first time will generate fortune need to fix to update every click

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,25 @@
+// function getFortune(url, button) {
+// let request = new XMLHttpRequest();
+
+// request.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//         callback(xmlHttp.responseText);
+//     }
+//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+//     xmlHttp.send(null);
+
+    // let data = JSON.parse(this.responseText);
+    // document.querySelector("[data-output]").innerHTML = data.fortune;
+    // };
+// }
+
 let request = new XMLHttpRequest();
+    let button = document.querySelector("[data-input]").addEventListener("click", function () {
+        // let buttonRequest = new XMLHttpRequest();
+        let data = JSON.parse(request.responseText);
+        document.querySelector("[data-output]").innerHTML = data.fortune;
+    });
 
-request.onreadystatechange = function () {
-    // console.log(response)
-    if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(this.responseText);
-    document.querySelector("[data-output]").innerHTML = data.fortune;
-    console.log(data)
-
-//     if (request.status >= 200 && request.status < 400) {
-//     data.forEach(function (fortune) {
-//         console.log(response);
-//     });
-// } else {
-//     console.log('error');
-    }
-}
-
-// let fortuneDiv = document.createElement('div')
-// fortuneDiv.textContent = request
-request.open('GET', 'http://localhost:3001/http://yerkee.com/api/fortune.txt', true);
+request.open('GET', 'http://localhost:3000/http://yerkee.com/api/fortune.txt', true);
 
 request.send();
