fb23d6c1b375967688b94294d83eb94a170dbf77
Anthony Moss <asmoss93@gmail.com>
2019-03-09 18:43:39 -0500

    adds attempt to add the request inside buttonclick function

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-// function getFortune(url, button) {
+// function getFortune(url, maybe put callback function here?) {
 // let request = new XMLHttpRequest();
 
 // request.onreadystatechange = function () {
 //     if (this.readyState == 4 && this.status == 200) {
 //         callback(xmlHttp.responseText);
 //     }
-//     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+//     xmlHttp.open("GET", 'http://localhost:3000/http://yerkee.com/api/fortune.txt', true); // true for asynchronous 
 //     xmlHttp.send(null);
 
     // let data = JSON.parse(this.responseText);
@@ -14,11 +14,12 @@
 // }
 
 let request = new XMLHttpRequest();
-    let button = document.querySelector("[data-input]").addEventListener("click", function () {
-        // let buttonRequest = new XMLHttpRequest();
-        let data = JSON.parse(request.responseText);
-        document.querySelector("[data-output]").innerHTML = data.fortune;
-    });
+
+let buttonClick = document.querySelector("[data-input]").addEventListener("click", function () {
+    // let request = new XMLHttpRequest();
+    let data = JSON.parse(request.responseText);
+    document.querySelector("[data-output]").innerHTML = data.fortune;
+});
 
 request.open('GET', 'http://localhost:3000/http://yerkee.com/api/fortune.txt', true);
 
