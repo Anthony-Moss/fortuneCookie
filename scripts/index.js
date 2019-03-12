d3b381ed3fde5ffd12b7e17b23575db3c7eb131d
Anthony Moss <asmoss93@gmail.com>
2019-03-11 22:04:34 -0400

    fixed button so it fetches on click. Also adds fortune to array, will use this for prev fortune in future

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,24 @@
-// function getFortune(url, maybe put callback function here?) {
-// let request = new XMLHttpRequest();
+let button = document.querySelector("[data-input");
+let outputElement = document.querySelector("[data-output")
+let url = 'http://my-little-cors-proxy.herokuapp.com/http://yerkee.com/api/fortune.txt';
+let fortunesArray = []
+let counter = 0
 
-// request.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         callback(xmlHttp.responseText);
-//     }
-//     xmlHttp.open("GET", 'http://localhost:3000/http://yerkee.com/api/fortune.txt', true); // true for asynchronous 
-//     xmlHttp.send(null);
-
-    // let data = JSON.parse(this.responseText);
-    // document.querySelector("[data-output]").innerHTML = data.fortune;
-    // };
-// }
-
-// let request = new XMLHttpRequest();
-
-// let buttonClick = document.querySelector("[data-input]").addEventListener("click", function () {
-//     // let request = new XMLHttpRequest();
-//     let data = JSON.parse(request.responseText);
-//     document.querySelector("[data-output]").innerHTML = data.fortune;
-// });
-
-// request.open('GET', 'http://localhost:3000/http://yerkee.com/api/fortune.txt', true);
-
-// request.send();
-
-fetch('http://my-little-cors-proxy.herokuapp.com/http://yerkee.com/api/fortune.txt')
+function getInfo(url) {
+    fetch('http://my-little-cors-proxy.herokuapp.com/http://yerkee.com/api/fortune.txt')
     .then(function (response) {
         return response.json();
     })
     .then(function (actualData) {
-        console.log(actualData)
-        let buttonClick = document.querySelector("[data-input]").addEventListener("click", function () {
-            // let request = new XMLHttpRequest();
-            // let data = JSON.parse(request.responseText);
-            document.querySelector("[data-output]").innerHTML = actualData.fortune;
-        });
-
+        outputElement.textContent = actualData.fortune
+        fortunesArray.push(actualData.fortune)
+        counter += 1
     });
+};
+
+// function getPrev(fortunesArray, counter) {
+//     outputElement.textContent = fortunesArray[counter -1]
+// }
+
+button.addEventListener('click', getInfo)
+// prevButton.addEventListener('click', getPrev)
